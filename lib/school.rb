6a9f37806503e188c34113788e42a5f613ecb35a
Jason Shockness <jshock12@gmail.com>
2019-07-17 21:33:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -19,7 +19,7 @@ class School
   
   def sort 
     sorted = {}
-    roster.each do |grade, student|
+    roster.each do |grade, students|
       sorted[grade] = sorted[student]
     end
     sorted
