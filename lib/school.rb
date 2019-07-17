1e4ca6ba5b29a764f6eba2282a81e2648630863f
Jason Shockness <jshock12@gmail.com>
2019-07-17 21:20:23 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,12 +5,12 @@ class School
   
   def initialize(name)
     @name = name
-    @roster = {} 
+    @roster = [] 
   end
   
   def add_student(student_name, grade)
-    roster['grade'] ||= {}
-    roster['grade'] << student_name
+    roster[grade] ||= []
+    roster[grade] << student_name
   end
   
   def grade(student_grade)
